4453637d99d696fa706037e2632d909a9db2f38a
Felix Schwickerath <felix@fschwickerath.de>
2023-04-12 16:37:16 +0200

    Add neofetch config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
+set -g fish_greeting
 if status is-interactive
     set -x GPG_TTY $(tty)
     set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 		ssh-add
+		clear
+		neofetch
 end
 fish_add_path /home/felix/.spicetify
